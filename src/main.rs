3ef295edd3c17fec66699ac34d9b5ae0cf5223ab
Daniele Pintore <daniele.pintore1@gmail.com>
2023-10-30 13:21:21 +0100

    Change tool name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process;
 
 fn main() {
-    if let Err(e) = xamarin_bundle_tools::run() {
+    if let Err(e) = xamarin_patcher::run() {
         eprintln!("Application error: {e}");
         process::exit(-1);   
     }
